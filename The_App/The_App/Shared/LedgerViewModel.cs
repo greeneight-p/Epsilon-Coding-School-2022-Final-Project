ad6654de2554ee7d9da60f7923213a77db6cb58d
Polyc <Polyc@JOHN-DESKTOP>
2022-04-15 12:15:44 +0300

    ledger start

---
+++ The_App/The_App/Shared/LedgerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static FuelStation.Models.Enumerations;
+
+namespace The_App.Shared {
+    public class LedgerViewModel {
+        public int Year { get; set; }
+        public MonthEnum Month { get; set; }
+        public decimal TotalIncome { get; set; }
+        public decimal TotalExpenses { get; set; }
+        public decimal Rent { get; set; }
+    }
+}
